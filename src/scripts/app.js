5c9ee6c46c28276cd9acf5dc84a710390decc2ef
shapirab <shapirab@jamd.ac.il>
2023-09-04 06:41:22 +0300

    4/9/23 init

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+import Game from "./game.js";
+
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('main-canvas');
+    let ctx = canvas.getContext('2d');
+    canvas.width = 1000;
+    canvas.height = 720;
+   
+    
+
+});
